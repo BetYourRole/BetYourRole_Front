4e31ec1288957b2d34c2e9f3a214313c579f0326
un-known0 <nam0221@naver.com>
2024-10-15 01:14:08 +0900

    feat: add Nav.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Nav from './components/Nav'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-
+      <Nav />
+      <Routes>
+      </Routes>
     </>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
+
+const Nav:React.FC = () => {
+    return (
+        <div className='flex justify-between px-16 py-8 items-center sticky w-full bg-white top-0'>
+                <Link to='/'><div className="logo text-2xl font-bold">Bet Your Task</div></Link>
+            <div className='menu flex gap-8'>
+                <Menu url="/" name="새 할일 생성"/>
+                <Menu url="/how-to-use" name="사용방법"/>
+                <Menu url="/login" name="로그인"/>
+            </div>
+        </div>
+    )
+}
+
+interface MenuProps{
+    url : string,
+    name : string
+}
+const Menu:React.FC<MenuProps> = ({ url, name }) =>{
+    const location = useLocation();
+    const isActive = location.pathname === url;
+
+    return(
+        <Link to={url}><div className={`text-l ${isActive ? "text-blue-500 font-bold " : ""}`}>{name}</div></Link>
+    )
+}
+
+export default Nav;
